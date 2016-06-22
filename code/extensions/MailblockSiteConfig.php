57349ee1f74a632f8aba08d8c2411d9159a31b53
Levi Brown <levi@signify.co.nz>
2016-06-22 17:42:27 +1200

    Add whitelist field to hidden fields.

--- code/extensions/MailblockSiteConfig.php
+++ code/extensions/MailblockSiteConfig.php
@@ -172,6 +172,7 @@ class MailblockSiteConfig extends DataExtension implements PermissionProvider {
 				$overrideConfiguration,
 				$recipients,
 				$advancedSettings,
+				$whitelist,
 			);
 			foreach ($hiddenFields as $field) {
 				$field->displayIf('MailblockEnabled')->isChecked();
