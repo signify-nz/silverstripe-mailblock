5d03539ad1fb34c246b3d7f729256c4370f1ad6d
Levi Wilson-Brown <levi@signify.co.nz>
2018-01-11 10:19:25 +1300

    Replace all tabs with spaces.

--- javascript/mailblock.js
+++ javascript/mailblock.js
@@ -1,16 +1,15 @@
 (function($) {
-	$(document).ready(function(){
-
-		showHideButton();
-		$('.cms-tabset-nav-primary li').on('click', showHideButton);
-		
-		function showHideButton() {
-			if ($('#Root_Mailblock_set').is(':visible')) {
-				$('#Form_EditForm_action_mailblockTestEmail').show();
-			}
-			else {
-				$('#Form_EditForm_action_mailblockTestEmail').hide();
-			}
-		}
-	})
+    $(document).ready(function(){
+        showHideButton();
+        $('.cms-tabset-nav-primary li').on('click', showHideButton);
+        
+        function showHideButton() {
+            if ($('#Root_Mailblock_set').is(':visible')) {
+                $('#Form_EditForm_action_mailblockTestEmail').show();
+            }
+            else {
+                $('#Form_EditForm_action_mailblockTestEmail').hide();
+            }
+        }
+    })
 })(jQuery);
